445569f69bb530d68e6634e373f5a79aa9163bbe
xiehuangbao <hxj8936270@vip.qq.com>
2017-08-27 19:15:25 +0800

    tcp client

---
+++ c2/tsTclnt3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from socket import *
+
+HOST = '10.0.0.100'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input('>')
+    if not data:
+        break
+    tcpCliSock.send(data)
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+
+tcpCliSock.close()
