d923d9ce1e5f7356a01f1f3c3fcf5e70a44d21de
xiehuangbao <hxj8936270@vip.qq.com>
2017-08-27 22:48:52 +0800

    udp客户端和服务器

---
+++ c2/tsUclnt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from socket import *
+
+HOST = '10.0.0.100'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+udpCliSock = socket(AF_INET, SOCK_DGRAM)
+
+while True:
+    data = input('>')
+    if not data:
+        break
+    udpCliSock.sendto(bytes(data, 'utf-8'), ADDR)
+    data, ADDR = udpCliSock.recvfrom(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+
+udpCliSock.close()
